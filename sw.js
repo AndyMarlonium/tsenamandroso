e0e2bd45e241bd295f66025cf2b9cac8f23fc036
AndyMarlonium <103562542+AndyMarlonium@users.noreply.github.com>
2026-09-19 21:54:44 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tsena-v5';
+const CACHE = 'tsena-v6';
 const FILES = ['./', './index.html', './app.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
